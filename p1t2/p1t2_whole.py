b042e92b64b0d0d9a54a220b77e9deeaf2a61ec7
Jingwei Liao <jl5983@columbia.edu>
2022-04-29 21:25:44 -0400

    p1t2 & Readme

--- p1t2/p1t2_whole.py
+++ p1t2/p1t2_whole.py
@@ -1,5 +1,6 @@
 ## initial Setup
 import re
+import regex
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, explode, lower
 from pyspark.sql.types import StringType, ArrayType
@@ -11,18 +12,22 @@ df = spark.read.format('xml').options(rowTag='page').load('hdfs:/enwiki_whole.xm
 
 ## internal link filter function
 def get_internal_link(text):
-    matches = re.findall(r'\[\[(.*?)\]\]', text)
-    for i, match in enumerate(matches):
-        matches[i] = re.split('\|', match)[0]
-
+    matches = regex.findall(r'\[\[((?:[^[\]]+|(?R))*+)\]\]', text)
     result = []
     for match in matches:
-        if len(re.findall(r'^category:', match)) > 0:
-            result.append(match)
-        elif len(re.findall(r':|#', match)) > 0:
-            continue
+        splited = re.split(r'\|', match)
+        temp = []
+        for link in splited:
+            if len(re.findall(r'^category:', link)) >0:
+                temp.append(link)
+            elif len(re.findall(r':|#', link)) > 0:
+                continue
+            else:
+                temp.append(link)
+        if len(temp)>0 :
+            result.append(temp[0]) 
         else:
-            result.append(match)
+            continue
     return result
 
 get_internal_link = udf(get_internal_link, ArrayType(StringType()))
@@ -34,9 +39,8 @@ def process(df):
         .dropna(how='any') \
         .withColumn('internal_link', get_internal_link('text')) \
         .select('title', explode('internal_link').alias('internal_link')) \
-        .sort('title', 'internal_link') \
-        .limit(5)
+        .sort('title', 'internal_link')
     return output
 
 whole_result = process(df)
-whole_result.coalesce(1).write.option("delimiter", "\t").csv("p1t2_whole.csv")
+whole_result.coalesce(1).write.option("delimiter", "\t").csv("gs://hw2-backup/p1t2_whole.csv")
